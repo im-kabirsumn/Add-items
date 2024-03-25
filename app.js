a56455a79009c986ec60105ab67f36053f3f8ee2
im-kabirsumn <158508809+im-kabirsumn@users.noreply.github.com>
2024-03-25 12:52:44 +0600

    script

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inputItemsForm = document.getElementById("input-items-form"); // add an event listener for submit event to get submitted data 
 const inputItems = document.getElementById("input-items");          // get it's value and push to local storage 
 const itemsContainer = document.getElementById("items-container");                     // you'll inject the item-list element to this
-// const itemList = document.getElementById("item-list");              // you'll craft this element and inject it to the itemsContainer element. also you'll add two event listener for click event to Strikethrough this item after one click (just add strike class) and delete this item after double click on this item.
+const listItems = document.querySelector("#items-container div");              // you'll craft this element and inject it to the itemsContainer element. also you'll add two event listener for click event to Strikethrough this item after one click (just add strike class) and delete this item after double click on this item.
 const deleteItem = document.getElementById("delete-item");          // add an event listener for click event to delete the entire items element
 
 
@@ -55,6 +55,8 @@ const createElement = (item) => {
   itemList.innerHTML = markup;
   itemList.append(liElm);
   itemsContainer.append(itemList);
+  console.log(listItems);
+
 };
 
 const reset = () => {
